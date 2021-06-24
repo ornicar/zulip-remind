f8bef1c78c24c99035a2481a25cd5b31f1ac1487
Thibault Duplessis <t@lichess.org>
2021-06-24 13:31:24 +0200

    don't break on unreadable redis entry

--- src/store.ts
+++ src/store.ts
@@ -17,11 +17,16 @@ export class RedisStore implements Store {
   poll = async () => {
     const entry = (await this.client.zrange(this.key, 0, 0))[0];
     if (entry) {
-      const add: Add = JSON.parse(entry);
-      add.when = new Date(add.when);
-      if (add && add.when < new Date()) {
+      try {
+        const add: Add = JSON.parse(entry);
+        add.when = new Date(add.when);
+        if (add.when < new Date()) {
+          await this.client.zrem(this.key, entry);
+          return add;
+        }
+      } catch {
+        console.log('Discarding unreadable entry', entry);
         await this.client.zrem(this.key, entry);
-        return add;
       }
     }
   };
